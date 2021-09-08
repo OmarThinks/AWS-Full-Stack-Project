19ec3df1aa1f778dcab2be87c2e3256ce17ffa07
Omar Magdy <contact.parmag@gmail.com>
2021-09-08 20:00:22 +0200

    created the product viewset

--- src/Backend/products/products/views.py
+++ src/Backend/products/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import (routers, viewsets)
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
