bfede3341c25bb77a662c605df348713132f9f99
Omar Magdy <contact.parmag@gmail.com>
2021-09-15 22:43:00 +0200

    added the products application

--- src/Backend/products/src/_app/settings.py
+++ src/Backend/products/src/_app/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'rest_framework',
-    #"products",
+    'rest_framework',
+    "products",
 ]
 
 MIDDLEWARE = [
